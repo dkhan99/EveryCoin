c85ec0289664247de2d2225822be430c385aa0eb
Spencer  Pearlman <spencerpearlman@spencers-mbp.attlocal.net>
2017-11-23 00:20:22 -0800

    Initial Commit

---
+++ OurCoin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OurCoin
+//
+//  Created by Spencer  Pearlman on 11/23/17.
+//  Copyright © 2017 USC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
